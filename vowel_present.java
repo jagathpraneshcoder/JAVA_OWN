91c4ffc97e9a94d59cbc7fce217c445ff41b951a
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2024-04-12 18:05:35 +0530

    Create vowel_present.java

---
+++ vowel_present.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String v = "aeiouAEIOU";
+        int vowel=0;
+        for(int i=0;i<s.length();i++){
+            char t = s.charAt(i);
+            if(v.indexOf(t)!=-1)
+            vowel+=1;
+        }
+        
+        if(vowel!=0)
+        System.out.println("vowel is present");
+        else
+        System.out.println("Not present");
+    }
+}
