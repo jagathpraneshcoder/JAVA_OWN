6b6b9d102a6da7d3be9dc7755b6122ac90bc2170
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2024-12-28 15:59:00 +0530

    Time: 3 ms (72.42%), Space: 44.3 MB (43.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,22 @@
 class Solution {
-    //Recursion
-    public boolean findPalindrome(String s,int i,int j){
-        if(i>j) return true;
-        else if(s.charAt(i)!=s.charAt(j)) return false;
-        return findPalindrome(s,i+1,j-1);
-    }
-
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
+        s=s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for(char c :  s.toCharArray()){
-            if(c>='a' && c<='z' || c>='0' && c<='9'){
+        for(char c : s.toCharArray()){
+            if(c>='a'&&c<='z' || c>='0'&&c<='9'){
                 sb.append(c);
             }
         }
-        //System.out.println(sb.toString());
-        //return true;
-        return findPalindrome(sb.toString(),0,sb.toString().length()-1);
+        s=sb.toString();
+        //System.out.println(s);
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
