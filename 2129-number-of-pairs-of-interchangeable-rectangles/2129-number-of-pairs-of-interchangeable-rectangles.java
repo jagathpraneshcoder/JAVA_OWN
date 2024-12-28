f5229df444c32110757135ad69b65d5d25a794b6
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2024-12-28 06:12:18 +0530

    Time: 48 ms (47.22%), Space: 98.6 MB (20.55%) - LeetHub

--- 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
@@ -1,19 +1,12 @@
 class Solution {
-    public long interchangeableRectangles(int[][] a) {
- 
-        long count = 0;
-        HashMap<Double,Integer> value = new HashMap<Double,Integer>();
-        for(int i=0 ; i < a.length ; i++){
-           double ratio = (double)a[i][0]/a[i][1];
-           //Find the Rtion Whether present or not
-           int freq = value.getOrDefault(ratio,0);
-            //Get the value in the freq
-           count+=(long)freq;
-            //Add the Freq in the count
-           value.put( ratio , freq + 1 );
-             //Increment the Frequency
+    public long interchangeableRectangles(int[][] rectangles) {
+        HashMap<Double,Integer> m = new HashMap<>();
+        long freq=0;
+        for(int i=0;i<rectangles.length;i++){
+            double value = (double) rectangles[i][0]/rectangles[i][1];
+            freq += m.getOrDefault( value , 0);
+            m.put(value , m.getOrDefault( value , 0 ) + 1);
         }
-       //If the unique ratio present the ratio doesnt repeatr else it will increment by one
-        return count;
+        return freq;
     }
 }
