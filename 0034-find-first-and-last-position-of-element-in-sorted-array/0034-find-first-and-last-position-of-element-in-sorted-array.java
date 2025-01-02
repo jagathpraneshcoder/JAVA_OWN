7b07e05270c394dd5fd914a9bd67e3be78547d22
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-01-02 06:52:31 +0530

    Time: 1 ms (17.49%), Space: 46.3 MB (6.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,45 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int first=Integer.MIN_VALUE,last=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                if(first==Integer.MIN_VALUE)
-                    first=i;
-                last=i;
-            }            
+    
+    public int lowerBound(int[] nums,int target){
+        int i=0,j=nums.length-1;
+        int mid=0;
+        int lb=nums.length;
+        while(i<=j){
+            mid=(i+j)/2;
+            if(nums[mid]>=target){
+                lb=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
         }
-        if(first==Integer.MIN_VALUE || last==Integer.MIN_VALUE)
-            return new int[]{-1,-1};  
-        return new int[]{first,last};
+        return lb;
+    }
+
+    public int upperBound(int[] nums,int target){
+        int i=0,j=nums.length-1;
+        int mid=0;
+        int ub=nums.length;
+        while(i<=j){
+            mid=(i+j)/2;
+            if(nums[mid]>target){
+                ub=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        return ub;
+    }
+    
+    public int[] searchRange(int[] nums, int target) {
+        int lb=lowerBound(nums,target);
+        System.out.println(lb);
+        if(lb==nums.length || nums[lb]!=target)
+            return new int[]{-1,-1};
+        int ub=upperBound(nums,target);
+        return new int[]{lb,ub-1};
     }
 }
