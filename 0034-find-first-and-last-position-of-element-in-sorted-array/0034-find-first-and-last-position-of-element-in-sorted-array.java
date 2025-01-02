3794a5e6b9ddfb4fcffe73c1dfea239045515595
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-01-02 07:39:05 +0530

    Time: 0 ms (100%), Space: 46 MB (36.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public int binarySearchFirst(int[] nums,int target){
+    public int binarySearch(boolean flag,int[] nums,int target){
         int left=0,right=nums.length-1;
         int res=-1;
         int mid = (left+right)/2;
@@ -8,27 +8,11 @@ class Solution {
             mid = (left+right)/2;
             if(nums[mid]==target){
                 res=mid;
-                right=mid-1;
-            }
-            else if(nums[mid]<target){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return res;
-    }
+                if(flag)
+                    right=mid-1;
+                else
+                    left=mid+1;
 
-    public int binarySearchLast(int[] nums,int target){
-        int left=0,right=nums.length-1;
-        int res=-1;
-        int mid = (left+right)/2;
-        while(left<=right){
-            mid = (left+right)/2;
-            if(nums[mid]==target){
-                res=mid;
-                left=mid+1;
             }
             else if(nums[mid]<target){
                 left=mid+1;
@@ -41,10 +25,10 @@ class Solution {
     }
     
     public int[] searchRange(int[] nums, int target) {
-        int first = binarySearchFirst(nums,target);
+        int first = binarySearch(true,nums,target);
         if(first==-1)
             return new int[]{-1,-1};
-        int last = binarySearchLast(nums,target);
+        int last = binarySearch(false,nums,target);
         //System.out.println(first+" "+last);
         return new int[]{first,last};
     }
