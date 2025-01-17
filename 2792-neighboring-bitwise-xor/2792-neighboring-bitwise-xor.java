7d6ba60711bdda02b3208f5f9daf6b4f5cebf4bb
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-01-17 18:39:40 +0530

    Time: 2 ms (100%), Space: 62.8 MB (18.24%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -9,4 +9,18 @@ class Solution {
         return sum==0;
         
     }
+
+    // Hints-02: The xor-sum of the derived array should be 0 since there is always a duplicate occurrence of each element.
+
+        // Example: Length of derived=4
+        //     derived[0]=org[0]^org[1]
+        //     derived[1]=org[1]^org[2]
+        //     derived[2]=org[2]^org[3]
+        //     derived[3]=org[3]^org[0]
+
+        //     Xor-Sum = derived[0] ^ derived[1] ^ derived[2] ^ derived[3]
+        //     Xor-Sum = (org[0]^org[1]) ^ (org[1]^org[2]) ^ (org[2]^org[3]) ^ (org[3]^org[0])
+        //     //re-arrange it.....
+        //     Xor-Sum = (org[0]^org[0]) ^ (org[1]^org[1]) ^ (org[2]^org[2]) ^ (org[3]^org[3])
+        //     Xor-Sum = 0
 }
