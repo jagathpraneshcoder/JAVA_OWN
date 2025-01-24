8ba8bd7536472bbc77e2644e2a27dc6a13e81541
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-01-24 13:26:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (59.56%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public static int solve(int i,int j,int[][] dp){
+        if(i==0 || j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j] = solve(i-1,j,dp)+solve(i,j-1,dp);
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+    for (int i = 0; i < m; i++) {
+        Arrays.fill(dp[i], -1);
+    }
+
+        return solve(m-1,n-1,dp);
+    }
+}
