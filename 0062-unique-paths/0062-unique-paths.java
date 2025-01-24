1a2263172f94f898420d233a8211426d7974bc89
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-01-24 21:16:29 +0530

    Time: 27 ms (2.49%), Space: 42 MB (6.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,31 @@
 class Solution {
 
-    public static int solve(int i,int j,int[][] dp){
-        if(i==0 || j==0)
-            return 1;
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        return dp[i][j] = solve(i-1,j,dp)+solve(i,j-1,dp);
+    public static int solve(int m,int n,int[][] dp){
+        dp[0][0] = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                 if(i==0 || j==0)
+                    dp[i][j]=1;
+                else if(i!=0 && j!=0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+        
     }
 
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-    for (int i = 0; i < m; i++) {
-        Arrays.fill(dp[i], -1);
-    }
-
-        return solve(m-1,n-1,dp);
-    }
+        dp[0][0] = 0;
+        int result = solve(m,n,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+            return result;
+        }
 }
