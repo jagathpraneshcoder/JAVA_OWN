4e70f061efa7a6c1ab02b89fbf8b1e2774921a9a
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-04-14 11:28:32 +0530

    Time: 0 ms (100%), Space: 45.4 MB (56.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,27 +1,11 @@
 class Solution {
-
-    static void sortArray(int[] arr){
-        int minIndex=0;
-        for(int i=0;i<arr.length-1;i++){
-            minIndex=i;
-            for(int j=i;j<arr.length;j++){
-                if(arr[j] < arr[minIndex]){
-                    minIndex = j;
-                }
-            }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
     public int missingNumber(int[] nums) {
-        Solution.sortArray(nums);
         int n = nums.length;
+        int Maxsum = (n*(n+1))/2;
+        int sum = 0;
         for(int i=0;i<n;i++){
-            if(nums[i]!=i){
-                return i;
-            }
+            sum+=nums[i];
         }
-        return n;
+        return Maxsum - sum;
     }
 }
