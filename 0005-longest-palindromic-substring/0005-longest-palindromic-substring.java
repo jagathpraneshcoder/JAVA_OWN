23b8c8e3f8519d76d7960764ca09f608eab55510
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-05-04 14:58:12 +0530

    Time: 14 ms (93.11%), Space: 42 MB (94.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,28 +2,28 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         int[] ans = new int[]{0,0};
-        for(int i=0;i<n;i++){
+        for(int i =0 ;i<n;i++){
+            
             int oddLength = expand(i,i,s);
-            if(oddLength > ans[1] - ans[0] + 1){
-                int dist = oddLength / 2;
-                ans[0] = i - dist;
-                ans[1] = i + dist;
-            } 
             int evenLength = expand(i,i+1,s);
-            if(evenLength > ans[1] - ans[0] + 1){
-                int dist = (evenLength / 2) - 1;
-                ans[0] = i - dist;
-                ans[1] = i + dist + 1;
-            } 
+            if(oddLength > ans[1]-ans[0]+1){
+                int index = oddLength / 2;
+                ans[0] = i - index;
+                ans[1] = i + index ;
+            }
+            if(evenLength > ans[1]-ans[0]+1){
+                int index = (evenLength / 2) - 1;
+                ans[0] = i - index;
+                ans[1] = i + index + 1;
+            }
+            
         }
         return s.substring(ans[0],ans[1]+1);
     }
-
     public int expand(int i,int j,String s){
-
-        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j)){
-                i--;
-                j++;
+        while(i>=0 && j < s.length() && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
         }
         return j - i - 1;
     }
