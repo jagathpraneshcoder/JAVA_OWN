b2132edd2b906be7a981168ade185ec0ac1552de
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-08-31 12:50:34 +0530

    Time: 0 ms (100%), Space: 40.1 MB (91.68%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int f0 = 0;
-        int f1 = 1;
-        if(n==0)
-            return f0;
-        if(n==1)
-            return f1;
-        int a = f0;
-        int b = f1;
-        int iterate = n;
-        int fn = -1;
-        while(iterate>=2){
-            fn = a + b;
+        if(n<2) return n;
+        int a=0,b=1,fn=0;
+        for(int i=2;i<=n;i++){
+            fn = a+b;
             a = b;
             b = fn;
-            iterate--;
         }
         return fn;
     }
